cbb715edf5bfe154c615860f19a5ffea0595e1d1
maximilianoangel.petrucci <maximilianoangel.petrucci@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-02 23:08:47 +0000

    Arreglados dos metodos mal escritos

--- test/fiuba/algo3/Policia/PoliciaTest.java
+++ test/fiuba/algo3/Policia/PoliciaTest.java
@@ -20,8 +20,8 @@ public class PoliciaTest {
     @Test
     public void calcularTiempoDeViajeCalculaDependiendoDelRangoDelPolicia() {
         Policia unPolicia = Policia("Juan");
-        Ciudad ciudadSalida = Ciudad("Buenos Aires", coodenada(-34.6158527, -58.4332985));
-        Ciudad ciudadDestino = Ciudad("Londres", coordenada(51.5286416,-0.1015987));
+        Ciudad ciudadSalida = Ciudad("Buenos Aires", Coodenada(-34.6158527, -58.4332985));
+        Ciudad ciudadDestino = Ciudad("Londres", Coordenada(51.5286416,-0.1015987));
         Rango novato = Novato();
         int tiempoDisponible;
 
