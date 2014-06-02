f37f5478f6b1228b364fb6f16dd500a3b82b6f12
maximilianoangel.petrucci <maximilianoangel.petrucci@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-02 23:55:47 +0000

    Hechos los metodos para la clase Policia

--- src/fiuba/algo3/Policia.java
+++ src/fiuba/algo3/Policia.java
@@ -1,28 +1,47 @@
 package fiuba.algo3;
 
 public class Policia {
-	private String nombre;
-	private Rango rango;
-	private int tiempoDisponible;
-	private Ciudad ciudadActual;
-	
-	public void Policia(String nombre){
-		this.nombre = nombre;
-	}
-	
-	public void setTiempoDisponible(int tiempo) {
-		this.tiempoDisponible = tiempo;
-	}
-	
-	public void setRango(Novato rango) {
-		this.rango = rango;
-	}
-	
-    public void setCiudadActual(Ciudad ciudadSalida) {
-		this.ciudadActual = ciudadSalida;
-	}
-	
+
+    private String nombre;
+    private int tiempoDisponible;
+    private Rango rango;
+    private Ciudad ciudadActual;
+
+    public Policia(String unNombre) {
+        this.nombre = unNombre;
+        this.tiempoDisponible = 154; //Tiempo en horas
+    }
+
+    public int getTiempoDisponible() {
+        return this.tiempoDisponible;
+    }
+
+    public void setRango(Rango unRango) {
+        this.rango = unRango;
+    }
+
+    public String solicitarPistaEconomia() {
+        return (this.ciudadActual).getPistaEconomia(this.rango);
+    }
+
+    public String solicitarPistaGeografiaHistoria() {
+        return (this.ciudadActual).getPistaGeografiaHistoria(this.rango);
+    }
+
+    public String solicitarPistaSimbolosPatrios() {
+        return (this.ciudadActual).getPistaSimbolosPatrios(this.rango);
+    }
+
+    public void setCiudadActual(Ciudad unaCiudad) {
+        this.ciudadActual = unaCiudad;
+    }
+
     public void viajar(Ciudad ciudadDestino) {
-	
-	}
+        Coordenada coordenadasCiudadActual;
+        int distanciaEnKM;
+        coordenadasCiudadActual = (this.ciudadActual).getCoordenadas();
+        distanciaEnKM = coordenadasCiudadActual.calcularDistancia(ciudadDestino.getCoordenadas());
+        this.tiempoDisponible -= (this.rango).calcularTiempoDeViaje(distanciaEnKM);
+        this.ciudadActual = ciudadDestino;
+    }
 }
