1a1da41668a3f03fa52c915a06eb250f0680dc24
maximilianoangel.petrucci <maximilianoangel.petrucci@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-02 22:39:48 +0000

    Arreglados las llamadas a metodos constructores. Cambiado la localizacion a la que se dirige el Detective (antes Biblioteca, ahora Aeropuerto). Cambiado el nombre del metodo de la clase Policia para ingresar en el Aeropuerto

--- trunk/test/fiuba/algo3/Policia/PoliciaTest.java
+++ trunk/test/fiuba/algo3/Policia/PoliciaTest.java
@@ -6,30 +6,32 @@ import org.junit.Assert;
 public class PoliciaTest {
     @Test
      public void getPistaDeberiaDevolverLaPistaAcordeAlRangoDelPolicia() {
-        Policia unPolicia = policia("Carlos");
-        Rango detective = detective();
-        Ciudad unaCiudad = ciudad("Buenos Aires", coordenada(1, 1));
-        Pista unaPista = pista("GeoFacil", "GeoInteredio", "GeoDificil");
+        Policia unPolicia = Policia("Carlos");
+        Rango detective = Detective();
+        Ciudad unaCiudad = Ciudad("Buenos Aires", Coordenada(1, 1));
+        Pista unaPista = Pista("AeroFacil", "AeroIntermedio", "AeroDificil");
 
         unPolicia.setRango(detective);
-        unaCiudad.setPistaGeografia(unaPista);
+        unaCiudad.setPistaAeropuerto(unaPista);
 
-        Assert.assertArrayEquals(policia.getPistaGeografia(), "GeoFacil");
+        Assert.assertArrayEquals(unPolicia.entrarAeropuerto(), "AeroIntermedio");
     }
     
     @Test
     public void calcularTiempoDeViajeCalculaDependiendoDelRangoDelPolicia() {
-        Policia unPolicia = policia("Juan");
-        Ciudad ciudadSalida = ciudad("Buenos Aires", coodenada(-34.6158527, -58.4332985));
-        Ciudad ciudadDestino = ciudad("Londres", coordenada(51.5286416,-0.1015987));
-        Rango novato = notavo();
+        Policia unPolicia = Policia("Juan");
+        Ciudad ciudadSalida = Ciudad("Buenos Aires", coodenada(-34.6158527, -58.4332985));
+        Ciudad ciudadDestino = Ciudad("Londres", coordenada(51.5286416,-0.1015987));
+        Rango novato = Novato();
+        int tiempoDisponible;
 
-        unPolicia.setTiempoDisponible(12);
         unPolicia.setRango(novato);
+        tiempoDisponible = unPolicia.getTiempoDisponible();
         unPolicia.setCiudadActual(ciudadSalida);
         unPolicia.viajar(ciudadDestino);
 
-        //11140,453 distancia en KM entre Buenos Aires y Londres segun http://www.freemaptools.com/measure-distance.htm y tarda 12HS
-        Assert.assertEquals(unPolicia.getTiempoDisponible(), 0);
+        /*11140,453 distancia en KM entre Buenos Aires y Londres segun http://www.freemaptools.com/measure-distance.htm
+        y tarda 12 HS viajando a 900km/h */
+        Assert.assertEquals(unPolicia.getTiempoDisponible(), (tiempoDisponible - 12));
     }
 }
