081d4ee31475fd7a599b58e182d6ea46ff41e947
mmeldiroch <mmeldiroch@gmail.com>
2014-06-03 21:06:27 +0000

    Correccion a la clase Coordenada y hechos sus test unitarios.

--- src/fiuba/algo3/Coordenada.java
+++ src/fiuba/algo3/Coordenada.java
@@ -2,18 +2,28 @@ package fiuba.algo3;
 
 import java.lang.Math;
 
+//final double radioTierra = 6371;
+
+
 public class Coordenada {
-	private double x;
-	private double y;
+	private double latitud;
+	private double longitud;
 	
-	public Coordenada(double x, double y) {
-		this.x = x;
-		this.y = y;
+	public Coordenada(double latitud, double longitud) {
+		this.latitud = latitud;
+		this.longitud = longitud;
 	}
 	
 	public double calcularDistancia(Coordenada otra) {
-        double distanciaXCuadrado = Math.pow(otra.x - this.x, 2);
-        double distanciaYCuadrado = Math.pow(otra.y - this.y, 2);
-		return Math.sqrt(distanciaXCuadrado + distanciaYCuadrado);
+        double difLatitud = Math.toRadians(otra.latitud - this.latitud);
+		double difLongitud = Math.toRadians(otra.longitud - this.longitud);
+		
+		double sinDifLatitud = Math.sin(difLatitud/2);
+		double sinDifLongitud = Math.sin(difLongitud/2);
+		
+		double aux = Math.pow(sinDifLatitud, 2) + Math.pow(sinDifLongitud, 2) * Math.cos(Math.toRadians(this.latitud)) * Math.cos(Math.toRadians(otra.latitud));
+		double aux1 = 2 * Math.atan2(Math.sqrt(aux), Math.sqrt(1 - aux));
+		
+		return 6371.00 * aux1;
 	}
 }
