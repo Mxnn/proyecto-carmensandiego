5fac73c4a82897aace3d646bdc17fe3ef3911777
maximilianoangel.petrucci <maximilianoangel.petrucci@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-03 03:26:37 +0000

    Agregados test para Policia y cambiada la ubicacion del la clase PoliciaTest

---
+++ test/fiuba/algo3/PoliciaTest.java
@@ -0,0 +1,37 @@
+package fiuba.algo3;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PoliciaTest {
+    @Test
+     public void getPistaDeberiaDevolverLaPistaAcordeAlRangoDelPolicia() {
+        Policia unPolicia = new Policia("Carlos");
+        Rango detective = new Detective();
+        Ciudad unaCiudad = new Ciudad("Buenos Aires", new Coordenada(-34.6158527, -58.4332985));
+        Pista unaPista = new Pista("SPFacil", "SPIntermedio", "SPDificil");
+
+        unPolicia.setRango(detective);
+        unaCiudad.setPistaSimbolosPatrios(unaPista);
+
+        Assert.assertEquals(unPolicia.solicitarPistaSimbolosPatrios(), "SPIntermedio");
+    }
+    
+    @Test
+    public void calcularTiempoDeViajeCalculaDependiendoDelRangoDelPolicia() {
+        Policia unPolicia = new Policia("Juan");
+        Ciudad ciudadSalida = Ciudad("Buenos Aires", new Coordenada(-34.6158527, -58.4332985));
+        Ciudad ciudadDestino = Ciudad("Londres", new Coordenada(51.5286416,-0.1015987));
+        Rango novato = new Novato();
+        int tiempoDisponible;
+
+        unPolicia.setRango(novato);
+        tiempoDisponible = unPolicia.getTiempoDisponible();
+        unPolicia.setCiudadActual(ciudadSalida);
+        unPolicia.viajar(ciudadDestino);
+
+        /*11140,453 distancia en KM entre Buenos Aires y Londres segun http://www.freemaptools.com/measure-distance.htm
+        y tarda 12 HS viajando a 900km/h */
+        Assert.assertEquals(unPolicia.getTiempoDisponible(), (tiempoDisponible - 12));
+    }
+}
