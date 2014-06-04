13c5cceda950ec28238c9dc308ce911847e3b9ec
flopy1706 <flopy1706@gmail.com>
2014-06-04 05:59:35 +0000

    Se cambio la forma en la que se pedian las pistas

--- src/fiuba/algo3/Sargento.java
+++ src/fiuba/algo3/Sargento.java
@@ -1,18 +1,18 @@
 package fiuba.algo3;
 
 public class Sargento implements Rango{
-	
+
 	private int velosidad;
-	
+
 	public Sargento(){
 		velosidad=1500;
 	}
 	public int calcularTiempoDeViaje (double distancia){
 		return (int) (distancia/ velosidad);
-		
+
 	}
 
-    public String obtenerPista(Pista pista) {
-        return pista.getDificil();
+    public void setDificultadPista(Pista pista) {
+        pista.setDificultadDificil();
     }
 }
