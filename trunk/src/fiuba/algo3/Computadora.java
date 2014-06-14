b8437f42d0f0b6a27a82073845ef8e74232beb48
mmeldiroch@gmail.com <mmeldiroch@gmail.com@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-14 03:18:57 +0000

    Añadida toda la logica de la clase Computadora y sus test

--- trunk/src/fiuba/algo3/Computadora.java
+++ trunk/src/fiuba/algo3/Computadora.java
@@ -19,4 +19,78 @@ public class Computadora {
 	public ArrayList<Ladron> getSospechosos() {
 		return sospechosos;
 	}
+	
+	public void filtrarCaracteristica(Ladron.Sexo sexo) {
+		ArrayList<Ladron> sospechososFiltrados = new ArrayList();
+		
+		int cantidadSospechosos = sospechosos.size();
+		for (int i = 0; i < cantidadSospechosos ; i++) {
+			Ladron ladron = sospechosos.get(i);
+			if (ladron.tieneEstaCaracteristica(sexo)) {
+				sospechososFiltrados.add(ladron);
+			}
+		}
+		
+		sospechosos = sospechososFiltrados;
+	}
+	
+	public void filtrarCaracteristica(Ladron.Pelo pelo) {
+		ArrayList<Ladron> sospechososFiltrados = new ArrayList();
+		
+		int cantidadSospechosos = sospechosos.size();
+		for (int i = 0; i < cantidadSospechosos ; i++) {
+			Ladron ladron = sospechosos.get(i);
+			if (ladron.tieneEstaCaracteristica(pelo)) {
+				sospechososFiltrados.add(ladron);
+			}
+		}
+		
+		sospechosos = sospechososFiltrados;
+	}
+	
+	public void filtrarCaracteristica(Ladron.Hobby hobby) {
+		ArrayList<Ladron> sospechososFiltrados = new ArrayList();
+		
+		int cantidadSospechosos = sospechosos.size();
+		for (int i = 0; i < cantidadSospechosos ; i++) {
+			Ladron ladron = sospechosos.get(i);
+			if (ladron.tieneEstaCaracteristica(hobby)) {
+				sospechososFiltrados.add(ladron);
+			}
+		}
+		
+		sospechosos = sospechososFiltrados;
+	}
+	
+	public void filtrarCaracteristica(Ladron.Auto auto) {
+		ArrayList<Ladron> sospechososFiltrados = new ArrayList();
+		
+		int cantidadSospechosos = sospechosos.size();
+		for (int i = 0; i < cantidadSospechosos ; i++) {
+			Ladron ladron = sospechosos.get(i);
+			if (ladron.tieneEstaCaracteristica(auto)) {
+				sospechososFiltrados.add(ladron);
+			}
+		}
+		
+		sospechosos = sospechososFiltrados;
+	}
+	
+	public void filtrarCaracteristica(Ladron.MarcaPersonal marcaPersonal) {
+		ArrayList<Ladron> sospechososFiltrados = new ArrayList();
+		
+		int cantidadSospechosos = sospechosos.size();
+		for (int i = 0; i < cantidadSospechosos ; i++) {
+			Ladron ladron = sospechosos.get(i);
+			if (ladron.tieneEstaCaracteristica(marcaPersonal)) {
+				sospechososFiltrados.add(ladron);
+			}
+		}
+		
+		sospechosos = sospechososFiltrados;
+	}
+	
+	public boolean puedoEmitirOrdenDeArresto() {
+		return (sospechosos.size() == 1) && (sospechosos.get(0) == buscado);
+	}
 }
