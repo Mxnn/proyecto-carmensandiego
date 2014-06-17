0b5a88cafe01dd8bd0c45cb42af1706643ba3c25
flopy1706@gmail.com <flopy1706@gmail.com@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-17 03:16:55 +0000

    Actualizacion de los la clase test de computadora y algunas modificaciones chicas en la clase computadora

--- trunk/test/fiuba/algo3/ComputadoraTest.java
+++ trunk/test/fiuba/algo3/ComputadoraTest.java
@@ -1,38 +1,126 @@
 package fiuba.algo3;
-/*
+
 import org.junit.Test;
+
+import fiuba.algo3.Ladron.Hobby;
+import fiuba.algo3.Ladron.Pelo;
+import fiuba.algo3.Ladron.Sexo;
 import junit.framework.Assert;
 
 public class ComputadoraTest {
     @Test
 
-    public void filtrarLadronDevuelveTrueSiElLadronFiltradoEsElBuscado() {
+    public void obtenerSospechososDevuelveUnaListaConSoloElLadronBuscadoSiNoSeAgregaronOtrosYNoHayFiltro() {
+        Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
+        Computadora computadora = new Computadora(buscado);
+
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+    }
+    @Test
+    public void obtenerSospechososDevuelveUnaListaConTodosLosSospechososSiNoHayFiltro() {
         Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
         Computadora computadora = new Computadora(buscado);
         Ladron sospechoso1 = new Ladron("Tomas", Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso2 = new Ladron("Graciela", Ladron.Sexo.FEMENINO, Ladron.Pelo.NEGRO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso3 = new Ladron("Carla", Ladron.Sexo.FEMENINO, Ladron.Pelo.RUBIO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
 
         computadora.setSospechoso(sospechoso1);
-        Assert.assertTrue(computadora.filtrarLadron(Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ));
+        computadora.setSospechoso(sospechoso2);
+        computadora.setSospechoso(sospechoso3);
+
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(1)== sospechoso1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(2)== sospechoso2);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(3)== sospechoso3);
     }
 
     @Test
-    public void filtrarLadronDevuelveFalseSiElLadronFiltradoNoEstaEnLaLista() {
+    public void obtenerSospechososDevuelveUnaListaDiferenteSiSeLeCambiaUnParametroDelMismoTipo() {
         Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
         Computadora computadora = new Computadora(buscado);
         Ladron sospechoso1 = new Ladron("Tomas", Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
 
         computadora.setSospechoso(sospechoso1);
-        Assert.assertFalse(computadora.filtrarLadron(Ladron.Sexo.FEMENINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ));
+        computadora.setCaracteritica (Pelo.NEGRO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+        Assert.assertFalse(computadora.obtenerSospechosos().get(0)== sospechoso1);
+        computadora.setCaracteritica (Pelo.RUBIO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== sospechoso1);
+        Assert.assertFalse(computadora.obtenerSospechosos().get(0)== buscado);
+
     }
 
     @Test
-    public void filtrarLadronDevuelveFalseSiElLadronFiltradoNoEsElBuscado() {
+    public void obtenerSospechososDevuelveUnaLIstaVaciaSiSeAgreganFiltrosQueNoCorrespondenANinguno() {
         Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
         Computadora computadora = new Computadora(buscado);
         Ladron sospechoso1 = new Ladron("Tomas", Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso2 = new Ladron("Graciela", Ladron.Sexo.FEMENINO, Ladron.Pelo.NEGRO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso3 = new Ladron("Carla", Ladron.Sexo.FEMENINO, Ladron.Pelo.RUBIO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
 
         computadora.setSospechoso(sospechoso1);
-        Assert.assertFalse(computadora.filtrarLadron(Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ));
+        computadora.setSospechoso(sospechoso2);
+        computadora.setSospechoso(sospechoso3);
+
+        computadora.setSospechoso(sospechoso1);
+        computadora.setCaracteritica (Pelo.NEGRO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 2);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(1)== sospechoso2);
+        computadora.setCaracteritica (Sexo.FEMENINO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== sospechoso2);
+        computadora.setCaracteritica (Hobby.CROQUET);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 0);
+
     }
+    @Test
+    public void emitirOrdenDeArrestoDevuelveTrueSiSoloQuedaUnSospechosoYEsElBuscado() {
+        Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
+        Computadora computadora = new Computadora(buscado);
+        Ladron sospechoso1 = new Ladron("Tomas", Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso2 = new Ladron("Graciela", Ladron.Sexo.FEMENINO, Ladron.Pelo.NEGRO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso3 = new Ladron("Carla", Ladron.Sexo.FEMENINO, Ladron.Pelo.RUBIO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
+
+        computadora.setSospechoso(sospechoso1);
+        computadora.setSospechoso(sospechoso2);
+        computadora.setSospechoso(sospechoso3);
+
+        computadora.setCaracteritica (Pelo.NEGRO);
+
+        computadora.setCaracteritica (Sexo.MASCULINO);
+        Assert.assertEquals(computadora.obtenerSospechosos().size(), 1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+        Assert.assertTrue(computadora.emitirOrdenDeArresto() );
+
+    }
+
+    @Test
+    public void emitirOrdenDeArrestoDevuelveFalseSiQuedamasDeUnSospechosoONoEsElBuscado() {
+        Ladron buscado = new Ladron("Jaime", Ladron.Sexo.MASCULINO, Ladron.Pelo.NEGRO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
+        Computadora computadora = new Computadora(buscado);
+        Ladron sospechoso1 = new Ladron("Tomas", Ladron.Sexo.MASCULINO, Ladron.Pelo.RUBIO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso2 = new Ladron("Graciela", Ladron.Sexo.FEMENINO, Ladron.Pelo.NEGRO, Ladron.Hobby.ALPINISMO, Ladron.Auto.MOTO, Ladron.MarcaPersonal.CICATRIZ);
+        Ladron sospechoso3 = new Ladron("Carla", Ladron.Sexo.FEMENINO, Ladron.Pelo.RUBIO, Ladron.Hobby.CROQUET, Ladron.Auto.CONVERTIBLE, Ladron.MarcaPersonal.CICATRIZ);
+
+        computadora.setSospechoso(sospechoso1);
+        computadora.setSospechoso(sospechoso2);
+        computadora.setSospechoso(sospechoso3);
+
+        computadora.setCaracteritica (Pelo.NEGRO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 2);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== buscado);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(1)== sospechoso2);
+        Assert.assertFalse(computadora.emitirOrdenDeArresto() );
+
+        computadora.setCaracteritica (Sexo.FEMENINO);
+        Assert.assertTrue(computadora.obtenerSospechosos().size()== 1);
+        Assert.assertTrue(computadora.obtenerSospechosos().get(0)== sospechoso2);
+        Assert.assertFalse(computadora.emitirOrdenDeArresto() );
+
+    }
+
+
 }
-*/
