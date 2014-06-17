d0b51c544cef3a71aae18a8b528fd635361d5da9
maximilianoangel.petrucci@gmail.com <maximilianoangel.petrucci@gmail.com@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-17 07:00:46 +0000

    Cambios en la implementacion y en los valores de devolucion de emitirOrdenDeArresto() (hay test que demuestran en que casos devuelve TRUE y en cuales FALSE) para la clase Computadora Todos los test de ComputadoraTest se hicieron correr

--- trunk/src/fiuba/algo3/Computadora.java
+++ trunk/src/fiuba/algo3/Computadora.java
@@ -3,65 +3,74 @@ package fiuba.algo3;
 import java.util.ArrayList;
 
 public class Computadora {
-	private Ladron sospechosoBuscado;
+    private Ladron ladronBuscado;
 	private ArrayList<Ladron> sospechosos;
     private Ladron.Sexo sexoDelLadron;
     private Ladron.Pelo peloDelLadron;
     private Ladron.Hobby hobbyDelLadron;
     private Ladron.Auto autoDelLadron;
     private Ladron.MarcaPersonal marcaPersonalDelLadron;
+    private ArrayList<Ladron> sospechososFiltrados;
 
-	public Computadora(Ladron buscado) {
-		this.sospechosoBuscado = buscado;
+	public Computadora(Ladron ladronBuscado) {
+        this.ladronBuscado = ladronBuscado;
 		this.sospechosos = new ArrayList<Ladron>();
-		this.sospechosos.add(buscado);
+        this.sospechosos.add(ladronBuscado);
+        this.sexoDelLadron = null;
+        this.peloDelLadron = null;
+        this.hobbyDelLadron = null;
+        this.autoDelLadron = null;
+        this.marcaPersonalDelLadron = null;
+        this.sospechososFiltrados = new ArrayList<Ladron>();
 	}
 
 	public void setSospechoso(Ladron ladron) {
 		this.sospechosos.add(ladron);
 	}
 
-	public void setCaracteritica (Ladron.Sexo sexo){
-        this.sexoDelLadron=sexo;
+	public void setCaracteristicaDelLadron (Ladron.Sexo sexo){
+        this.sexoDelLadron = sexo;
 	}
 
-	public void setCaracteritica (Ladron.Pelo pelo){
-        this.peloDelLadron=pelo;
+	public void setCaracteristicaDelLadron (Ladron.Pelo pelo){
+        this.peloDelLadron = pelo;
 	}
 
-    public void setCaracteritica (Ladron.Hobby hobby){
-        this.hobbyDelLadron=hobby;
+    public void setCaracteristicaDelLadron (Ladron.Hobby hobby){
+        this.hobbyDelLadron = hobby;
 	}
 
-    public void setCaracteritica (Ladron.Auto auto){
-        this.autoDelLadron=auto;
+    public void setCaracteristicaDelLadron (Ladron.Auto auto){
+        this.autoDelLadron = auto;
 	}
 
-    public void setCaracteritica (Ladron.MarcaPersonal marcaPersonal){
-        this.marcaPersonalDelLadron=marcaPersonal;
+    public void setCaracteristicaDelLadron (Ladron.MarcaPersonal marcaPersonal){
+        this.marcaPersonalDelLadron = marcaPersonal;
 	}
 
-    public  ArrayList<Ladron> obtenerSospechosos() {
-        ArrayList<Ladron> temp = new ArrayList<Ladron>();
+    private boolean ladronCumpleConLosFiltros(Ladron ladron) {
+        return (sexoDelLadron == null || ladron.tieneEstaCaracteristica(sexoDelLadron)) &&
+                (peloDelLadron == null ||  ladron.tieneEstaCaracteristica(peloDelLadron) )&&
+                (hobbyDelLadron == null || ladron.tieneEstaCaracteristica(hobbyDelLadron)) &&
+                (autoDelLadron == null || ladron.tieneEstaCaracteristica(autoDelLadron) )&&
+                (marcaPersonalDelLadron == null || ladron.tieneEstaCaracteristica(marcaPersonalDelLadron));
+    }
+
+    private void filtrarSospechosos() {
+        this.sospechososFiltrados.clear();
         for(Ladron ladron: this.sospechosos) {
-            if ((sexoDelLadron==null || ladron.tieneEstaCaracteristica(sexoDelLadron)) &&
-                (peloDelLadron==null ||  ladron.tieneEstaCaracteristica(peloDelLadron) )&&
-                (hobbyDelLadron==null || ladron.tieneEstaCaracteristica(hobbyDelLadron)) &&
-                (autoDelLadron==null || ladron.tieneEstaCaracteristica(autoDelLadron) )&&
-                (marcaPersonalDelLadron==null || ladron.tieneEstaCaracteristica(marcaPersonalDelLadron) ) ){
-                temp.add(ladron);
+            if (ladronCumpleConLosFiltros(ladron)){
+                this.sospechososFiltrados.add(ladron);
             }
         }
+    }
 
-        return temp;
+    public ArrayList<Ladron> getSospechososFiltrados() {
+        return this.sospechososFiltrados;
     }
 
     public boolean emitirOrdenDeArresto() {
-        sospechosos=obtenerSospechosos();
-        if (sospechosos.size() ==1){
-          return (sospechosos.get(0) == this.sospechosoBuscado);
-        }
-        return false;
-
+        filtrarSospechosos();
+        return (sospechososFiltrados.size() == 1 && sospechososFiltrados.get(0) == ladronBuscado);
     }
 }
