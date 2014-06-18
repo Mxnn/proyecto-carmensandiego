021cfa39c4b476991f2bdf0cd81933443f73b211
flopy1706 <flopy1706@gmail.com>
2014-06-18 07:00:53 +0000

    Se agrego una lista de ciudades y se crearon los metodos de hidratar

--- src/fiuba/algo3/Ciudad.java
+++ src/fiuba/algo3/Ciudad.java
@@ -1,6 +1,11 @@
 package fiuba.algo3;
-
+import java.awt.geom.Arc2D.Double;
 import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 
 public class Ciudad {
 
@@ -79,4 +84,42 @@ public class Ciudad {
     public boolean getEscondeAlLadron() {
         return this.acaEstaElLadron;
     }
+	
+	public static Ciudad hidratar(Node elementoCiudad) {
+		String nombre=((Element)elementoCiudad).getAttribute("nombre");
+
+		String stringCoordenadaX = ((Element)elementoCiudad).getAttribute("CoordenadaX");
+		int coordenadaX=Integer.parseInt(stringCoordenadaX);
+		String stringCoordenadaY = ((Element)elementoCiudad).getAttribute("CoordenadaY");
+		int coordenadaY=Integer.parseInt(stringCoordenadaY);
+		Ciudad ciudad= new Ciudad(nombre,new Coordenada((double)coordenadaX, (double) coordenadaY));
+		return ciudad;
+		}
+		
+/*		Esto no sirve pero lo guarde por las dudas es codigo q estaba probando
+		int coordenadaX = Integer.parseInt(ciudad.getAttribute("CoordenadaX"));
+		int coordenadaY = Integer.parseInt(ciudad.getAttribute("CoordenadaY"));
+		Coordenada coordenada=new Coordenada((double)coordenadaX, (double) coordenadaY);
+		
+		Ciudad nuevoCiudad = new Ciudad(listaDeCiudades.get(posicionDeLaLista), coordenada);
+
+		Pista pistaFE= Pista.hidratar((Element)doc.getElementsByTagName("pistaFE").item(0));
+		Pista pistaIE= Pista.hidratar((Element)doc.getElementsByTagName("pistaIE").item(0));
+		Pista pistaDE= Pista.hidratar((Element)doc.getElementsByTagName("pistaDE").item(0));
+		Pista pistaFT= Pista.hidratar((Element)doc.getElementsByTagName("pistaFT").item(0));
+		Pista pistaIT= Pista.hidratar((Element)doc.getElementsByTagName("pistaIT").item(0));
+		Pista pistaDT= Pista.hidratar((Element)doc.getElementsByTagName("pistaDT").item(0));
+		Pista pistaFC= Pista.hidratar((Element)doc.getElementsByTagName("pistaFC").item(0));
+		Pista pistaIC= Pista.hidratar((Element)doc.getElementsByTagName("pistaIC").item(0));
+		Pista pistaDC= Pista.hidratar((Element)doc.getElementsByTagName("pistaDC").item(0));
+		Ciudad nuevoCiudad = new Ciudad();
+		Element elementoEquipo = (Element)doc.getElementsByTagName("Equipo").item(0);
+		nuevoEquipo.nombre = elementoEquipo.getAttribute("nombre");
+		Element elementoJugadores = (Element)doc.getElementsByTagName("Jugadores").item(0);
+		for(int i=0; i< elementoJugadores.getChildNodes().getLength(); i++) {
+			Jugador jugador = Jugador.hidratar(elementoJugadores.getChildNodes().item(i));
+			nuevoEquipo.agregarJugador(jugador);
+		}
+		return nuevoEquipo;*/
+	
 }
