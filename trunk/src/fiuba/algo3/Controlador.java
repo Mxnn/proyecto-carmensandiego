201c72d801aa9458b89a4aec8cf4be403a971b5a
flopy1706@gmail.com <flopy1706@gmail.com@06f7eda2-a0b2-e3b3-d0a3-af2f5b304731>
2014-06-24 21:07:30 +0000

    Se cambio la organizacion y se divieron para no romper el encampsulamiento de la clase vista y controlador

--- trunk/src/fiuba/algo3/Controlador.java
+++ trunk/src/fiuba/algo3/Controlador.java
@@ -1,22 +1,218 @@
 package fiuba.algo3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controlador {
 	
-	
-	
 	private Scanner scan;
-
-	Controlador (){
-		this.scan= new Scanner(System.in);
-	}
+	private Juego juego;
+	private Vista vista;
 	
-	 int edad = scan.nextInt();
+	Controlador (Juego juego){
+		this.scan= new Scanner(System.in);
+		this.juego=juego;
+		this.vista=new Vista(this);
+	}
 
 	public int pedirOpcion() {
 
 		return scan.nextInt();
+	}
+	
+	public void elegir_opcion_filtro() throws ExcepcionJugadorSinTiempoDisponible{
+		
+		int opcion=pedirOpcion();
+		if (opcion==1){
+			pedirOpcionFiltrarSexo();
+		}
+		else if (opcion==2){
+			pedirOpcionFiltrarPelo();		
+		}
+		else if (opcion==3){
+			pedirOpcionFiltrarHobby();		
+		}		
+		else if (opcion==4){
+			pedirOpcionFiltrarAuto();		
+		}	
+		else if (opcion==5){
+			pedirOpcionFiltrarMarcaPersonal();		
+		}
+		else if (opcion==0){
+			pedirOpcionComputadora();		
+		}
+	}
+
+	void pedirOpcionComputadora() throws ExcepcionJugadorSinTiempoDisponible {
+		int opcion=pedirOpcion();
+		if (opcion==1){
+			vista.mostrarOpcionFiltrar();
+		}
+		else if (opcion==2){
+			pedirOpcionOrdenDeArresto();		
+		}
+		else if (opcion==0){
+			pedirOpcionMenu();
+		}
+	}
+
+	void pedirOpcionMenu() throws ExcepcionJugadorSinTiempoDisponible {
+		int opcion=pedirOpcion();
+		
+		if (opcion==1){
+			vista.mostrarOpcionPista();
+		}
+		else if (opcion==2){
+			vista.mostrarOpcionViajar(juego.obtenerNombreDeLasCiudadesALasQuePuedoIr());
+		}
+		else if (opcion==3){
+			vista.mostrarOpcionComputadora();		
+		}
+	}
+
+	private void pedirOpcionPista() throws ExcepcionJugadorSinTiempoDisponible {
+		// TODO Auto-generated method stub
+		int opcion=pedirOpcion();
+		
+		if (opcion==1){
+				vista.imprimirPista(juego.verPistaEconomia());
+		}
+		else if (opcion==2){
+			vista.imprimirPista(juego.verPistaTransporte());
+		}
+		else if (opcion==3){
+			vista.imprimirPista(juego.verPistaCultural());
+		}	
+		vista.mostrarMenu();
+	}
+
+	void pedirOpcionViajar(ArrayList<String> NombreDeLasCiudadesALasQuePuedoIr) throws ExcepcionJugadorSinTiempoDisponible {
+		int opcion=pedirOpcion();
+		
+		juego.viajar(NombreDeLasCiudadesALasQuePuedoIr.get(opcion-1));
+	}
+
+	 void pedirOpcionOrdenDeArresto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	 void pedirOpcionFiltrar() throws ExcepcionJugadorSinTiempoDisponible {
+	
+			int opcion=pedirOpcion();
+			
+			if (opcion==1){
+				vista.mostrarOpcionFiltrarSexo();
+			}
+			else if (opcion==2){
+				vista.mostrarOpcionFiltrarPelo();		
+			}
+			else if (opcion==3){
+				vista.mostrarOpcionFiltrarHobby();		
+			}		
+			else if (opcion==4){
+				vista.mostrarOpcionFiltrarAuto();		
+			}	
+			else if (opcion==5){
+				vista.mostrarOpcionFiltrarMarcaPersonal();		
+			}
+			else if (opcion==0){
+				vista.mostrarOpcionComputadora();		
+			}
+	}
+
+     void pedirOpcionFiltrarMarcaPersonal() throws ExcepcionJugadorSinTiempoDisponible {
+ 		int opcion=pedirOpcion();			
+		
+ 		if (opcion==1){
+ 			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.ANILLO);
+ 		}
+ 		else if (opcion==2){
+ 			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.CICATRIZ);	
+ 		}	
+ 		else if (opcion==3){
+ 			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.JOYAS);		
+ 		}	
+ 		else if (opcion==4){
+ 			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.TATUAJE);		
+ 		}	
+ 		else if (opcion==0){
+ 			vista.mostrarOpcionFiltrar();		
+ 		}				
+	}
+
+	 void pedirOpcionFiltrarAuto() throws ExcepcionJugadorSinTiempoDisponible {
+		int opcion=pedirOpcion();			
+			
+		if (opcion==1){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.CONVERTIBLE);
+		}
+		else if (opcion==2){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.LIMUSINA);	
+		}	
+		else if (opcion==3){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.MOTO);		
+		}	
+		else if (opcion==0){
+			vista.mostrarOpcionFiltrar();		
+		}		
+	}
+
+	 void pedirOpcionFiltrarHobby() throws ExcepcionJugadorSinTiempoDisponible {
+		int opcion=pedirOpcion();			
+	
+		if(opcion==1){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.ALPINISMO);
+		}
+		else if (opcion==2){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.CROQUET);	
+		}	
+		else if (opcion==3){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.TENNIS);		
+		}	
+		else if (opcion==0){
+			vista.mostrarOpcionFiltrar();		
+		}				
+	}
+
+	void pedirOpcionFiltrarPelo() throws ExcepcionJugadorSinTiempoDisponible {
+
+		int opcion=pedirOpcion();
+		
+		if (opcion==1){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.NEGRO);
+		}
+		else if (opcion==2){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.RUBIO);	
+		}
+		else if (opcion==3){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.ROJO);		
+		}		
+		else if (opcion==4){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.MARRON);	
+		}
+		else if (opcion==0){
+			vista.mostrarOpcionFiltrar();		
+		}		
+	}
+
+	void pedirOpcionFiltrarSexo() throws ExcepcionJugadorSinTiempoDisponible {
+
+		int opcion=pedirOpcion();	
+		
+		if (opcion==1){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Sexo.MASCULINO);
+		}
+		else if (opcion==2){
+			juego.ingresarCaracteristicaDelLadron(Ladron.Sexo.FEMENINO);	
+		}	
+		else if (opcion==0){
+			vista.mostrarOpcionFiltrar();		
+		}		
+	}
+
+	public String getTiempoDisponible() {
+		return juego.getTiempoDisponible();
 	}
 
 }
--- trunk/src/fiuba/algo3/Vista.java
+++ trunk/src/fiuba/algo3/Vista.java
@@ -4,12 +4,11 @@ import java.util.*;
 
 public class Vista {
 	
-	private Juego juego;
 	private Controlador controlador; 
 
-	public Vista(Juego juego, Controlador controlador)
+	public Vista( Controlador controlador)
 	{	
-		this.juego=juego;
+
 		this.controlador=controlador;
 	}
 	
@@ -22,37 +21,17 @@ public class Vista {
 		System.out.println ("3. Acceder a la computadora.");
 		System.out.println ("4. Salir.");		
 		System.out.println ("Ingrese el numero de la opcion que desee");
-		int opcion=controlador.pedirOpcion();
-		
-		if (opcion==1){
-			mostrarOpcionPista();
-		}
-		else if (opcion==2){
-			mostrarOpcionViajar(juego.obtenerNombreDeLasCiudadesALasQuePuedoIr());
-		}
-		else if (opcion==3){
-			mostrarOpcionComputadora();		
-		}
+		controlador.pedirOpcionMenu();
 	}
 
-	private void mostrarOpcionComputadora() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionComputadora() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que desea hacer?");
 		System.out.println ("1. Filtrar sospechosos.");		
 		System.out.println ("2. Pedir orden de arresto.");				
 		imprimirOpcionRegresar();
 		System.out.println ("Ingrese el numero de la opcion que desee");	
-		int opcion=controlador.pedirOpcion();
-		
-		if (opcion==1){
-		
-			mostrarOpcionFiltrar();
-		}
-		else if (opcion==2){
-			mostrarOpcionOrdenDeArresto();		
-		}
-		else if (opcion==0){
-			mostrarMenu();
-		}
+
+		controlador.pedirOpcionComputadora();
 	}
 
 	private void mostrarOpcionOrdenDeArresto() {
@@ -60,7 +39,7 @@ public class Vista {
 		
 	}
 
-	private void mostrarOpcionFiltrar() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrar() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que tipo de caracteristica del ladron desea filtrar?");
 		System.out.println ("1. Sexo.");		
 		System.out.println ("2. Pelo.");				
@@ -69,146 +48,67 @@ public class Vista {
 		System.out.println ("5. MarcaPersonal.");
 		imprimirOpcionRegresar();
 		System.out.println ("Ingrese el numero de la opcion que desee");	
-		int opcion=controlador.pedirOpcion();
 		
-		if (opcion==1){
-			mostrarOpcionFiltrarSexo();
-		}
-		else if (opcion==2){
-			mostrarOpcionFiltrarPelo();		
-		}
-		else if (opcion==3){
-			mostrarOpcionFiltrarHobby();		
-		}		
-		else if (opcion==4){
-			mostrarOpcionFiltrarAuto();		
-		}	
-		else if (opcion==5){
-			mostrarOpcionFiltrarMarcaPersonal();		
-		}
-		else if (opcion==0){
-			mostrarOpcionComputadora();		
-		}
+		controlador.pedirOpcionFiltrar();
+
 	}
 
-	private void mostrarOpcionFiltrarMarcaPersonal() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrarMarcaPersonal() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que tipo de marca personal tiene el ladron?");
 		System.out.println ("1. Anillo.");		
 		System.out.println ("2. Cicatriz.");				
 		System.out.println ("3. Joyas.");	
 		System.out.println ("4. Tatuaje.");		
 		imprimirOpcionRegresar();	
-		int opcion=controlador.pedirOpcion();			
 		
-		if (opcion==1){
-			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.ANILLO);
-		}
-		else if (opcion==2){
-			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.CICATRIZ);	
-		}	
-		else if (opcion==3){
-			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.JOYAS);		
-		}	
-		else if (opcion==4){
-			juego.ingresarCaracteristicaDelLadron(Ladron.MarcaPersonal.TATUAJE);		
-		}	
-		else if (opcion==0){
-			mostrarOpcionFiltrar();		
-		}				
+		controlador.pedirOpcionFiltrarMarcaPersonal();
+
 		
 	}
 
-	private void mostrarOpcionFiltrarAuto() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrarAuto() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que tipo de auto tiene el ladron?");
 		System.out.println ("1. Convertible.");		
 		System.out.println ("2. Limusina.");				
 		System.out.println ("3. Moto.");
-		imprimirOpcionRegresar();			
-		int opcion=controlador.pedirOpcion();			
-		
-		if (opcion==1){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.CONVERTIBLE);
-		}
-		else if (opcion==2){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.LIMUSINA);	
-		}	
-		else if (opcion==3){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Auto.MOTO);		
-		}	
-		else if (opcion==0){
-			mostrarOpcionFiltrar();		
-		}		
+		imprimirOpcionRegresar();	
+		controlador.pedirOpcionFiltrarAuto();
 	}	
 		
-	
 
-	private void mostrarOpcionFiltrarHobby() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrarHobby() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que tipo de hobby tiene el ladron?");
 		System.out.println ("1. Alpinismo.");		
 		System.out.println ("2. Croquet.");				
 		System.out.println ("3. Tennis.");
 		imprimirOpcionRegresar();			
-		int opcion=controlador.pedirOpcion();			
+		controlador.pedirOpcionFiltrarHobby();
 		
-		if (opcion==1){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.ALPINISMO);
-		}
-		else if (opcion==2){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.CROQUET);	
-		}	
-		else if (opcion==3){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Hobby.TENNIS);		
-		}	
-		else if (opcion==0){
-			mostrarOpcionFiltrar();		
-		}		
+
 	}
 
-	private void mostrarOpcionFiltrarSexo() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrarSexo() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("De que sexo es el ladron?");
 		System.out.println ("1. Masculino.");		
 		System.out.println ("2. Femenino.");
 		imprimirOpcionRegresar();			
-		int opcion=controlador.pedirOpcion();	
-		
-		if (opcion==1){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Sexo.MASCULINO);
-		}
-		else if (opcion==2){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Sexo.FEMENINO);	
-		}	
-		else if (opcion==0){
-			mostrarOpcionFiltrar();		
-		}		
+		controlador.pedirOpcionFiltrarSexo();
+
 	}
 
-	private void mostrarOpcionFiltrarPelo() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionFiltrarPelo() throws ExcepcionJugadorSinTiempoDisponible {
 		System.out.println ("Que color de pelo tiene el ladron?");
 		System.out.println ("1. Negro.");		
 		System.out.println ("2. Rubio.");				
 		System.out.println ("3. Rojo.");	
 		System.out.println ("4. Marron.");	
 		imprimirOpcionRegresar()	;
-		int opcion=controlador.pedirOpcion();
 		
-		if (opcion==1){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.NEGRO);
-		}
-		else if (opcion==2){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.RUBIO);	
-		}
-		else if (opcion==3){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.ROJO);		
-		}		
-		else if (opcion==4){
-			juego.ingresarCaracteristicaDelLadron(Ladron.Pelo.MARRON);	
-		}
-		else if (opcion==0){
-			mostrarOpcionFiltrar();		
-		}		
+		controlador.pedirOpcionFiltrarPelo();
+
 	}
 
-	private void mostrarOpcionViajar(ArrayList<String> nombresCiudades) throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionViajar(ArrayList<String> nombresCiudades) throws ExcepcionJugadorSinTiempoDisponible {
 		imprimirCiudadActual();
 		imprimirTiempoDisponible();			
 		System.out.println ("A donde desea viajar?");	
@@ -218,12 +118,10 @@ public class Vista {
 			numeroOpcion+=1;
 		}
 		imprimirOpcionRegresar();	
-		int opcion=controlador.pedirOpcion();
-		
-		juego.viajar(nombresCiudades.get(opcion-1));
-		}
+		controlador.pedirOpcionViajar(nombresCiudades) ;
+	}
 
-	private void mostrarOpcionPista() throws ExcepcionJugadorSinTiempoDisponible {
+	void mostrarOpcionPista() throws ExcepcionJugadorSinTiempoDisponible {
 		imprimirCiudadActual();
 		imprimirTiempoDisponible();
 		System.out.println ("Que lugar desea visitar?");
@@ -232,26 +130,18 @@ public class Vista {
 		System.out.println ("3. Museo.");
 		imprimirOpcionRegresar()	;
 		System.out.println ("Ingrese el numero de la opcion que desee");
-		int opcion=controlador.pedirOpcion();
-		
-		if (opcion==1){
-			System.out.println(juego.verPistaEconomia());
-		}
-		else if (opcion==2){
-			System.out.println(juego.verPistaTransporte());
-		}
-		else if (opcion==3){
-			System.out.println(juego.verPistaCultural());
-		}	
-		mostrarMenu();
 	}
 
+	public void imprimirPista(String pista) {
+		System.out.println (pista);
+	}	
+	
 	private void imprimirTiempoDisponible() {
-		System.out.println ("Tiempo Disponible:"+juego.getTiempoDisponible ());
+		System.out.println ("Tiempo Disponible:"+controlador.getTiempoDisponible ());
 	}
 	
 	private void imprimirCiudadActual() {
-		System.out.println ("Ciudad Actual:"+juego.getTiempoDisponible ());
+		System.out.println ("Ciudad Actual:"+controlador.getTiempoDisponible ());
 	}
 		
 	private void imprimirOpcionRegresar() {	
